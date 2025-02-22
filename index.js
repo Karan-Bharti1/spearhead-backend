7db2fe69e5c40b7c31404233f300a71542dd1293
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-22 13:15:11 +0530

    chore: added middlewares and sorted cors options

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const {nitialiseDatabase, initialiseDatabase}=require("./database/database.connection")
+const cors=require("cors")
 const Lead=require("./models/Lead")
 const Comment=require("./models/Comment")
 const Sales=require("./database/database.connection")
 const Tag=require("./database/database.connection")
+const express=require('express')
+const app=express()
+app.use(express.json())
+const corsOptions={
+    origin:"*",
+    credentials:true,
+    optionsSuccessStatus:200
+}
+app.use(cors(corsOptions))
 initialiseDatabase()
