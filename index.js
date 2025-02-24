9fe3561df29279667883483ab4e31f7416a35992
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-24 08:34:58 +0530

    refactor: refactored code for api to get all lead data with improved error handling

--- index.js
+++ index.js
@@ -97,12 +97,18 @@ console.log(error)
 })
 app.get("/leads",async (req,res) => {
     try {
+        const { salesAgent, status, tags, source } = req.query;
         const data=await Lead.find().populate("salesAgent")
         if(data && data.length>0){
             res.status(200).json(data)
         }else{
             res.status(404).json({"error":"Leads data not found."})
         } 
+        if (status && !allowedStatuses.includes(status)) {
+            return res.status(400).json({ 
+              "error": `Invalid input: 'status' must be one of ${JSON.stringify(allowedStatuses)}.` 
+            });
+          }
     } catch (error) {
         res.status(500).json({"error":"Failed to get Leads data"})
     }
@@ -141,6 +147,7 @@ res.status(200).json({
         res.status(500).json({"error":"Failed to update lead data"})
     }
 })
+
 app.listen(PORT,()=>{
     console.log("Server is running on PORT: ",PORT)
 })
